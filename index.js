11fc67fc939971c6e8912a279c7341d76553e3eb
Metehan Erzincanli <65494091+v01d@users.noreply.github.com>
2022-07-04 18:12:35 +0300

    Commited initial files

---
+++ index.js
@@ -0,0 +1,5 @@
+const {generatePassword} = require('./generatePassword.js')
+const {generateCode} = require('./generateCode.js')
+
+module.exports.generatePassword = generatePassword;
+module.exports.generateCode = generateCode;
